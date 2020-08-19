d23f3a7b40e2969a96cbbd940b4c77e546843d0e
Luis Eduardo Nunez Altamirano <a01633894@itesm.mx>
2020-08-18 20:20:26 -0500

    Update Demo

--- labs/c-basics/fahrenheit_celsius.c
+++ labs/c-basics/fahrenheit_celsius.c
@@ -11,7 +11,7 @@ int main()
     int fahr;
 
     for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
-	printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
-	printf("Hello World")
+	    printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+	    printf("Hello World");
     return 0;
 }
