af84e6a01ad56a1e4f060ff7e7430be158481fb4
Luis Eduardo Nunez Altamirano <a01633894@itesm.mx>
2020-08-30 17:08:37 -0500

    Word-reverser lab solved
    Signed-off-by: Luis Eduardo Nunez Altamirano <a01633894@itesm.mx>

--- labs/word-reverser/reverser.c
+++ labs/word-reverser/reverser.c
@@ -2,5 +2,24 @@
 
 int main(){
     // Place your magic here
+
+    int c;
+    int words[100];
+    int pos = 0;
+    while ((c = getchar()) != EOF){
+        if (c == '\n'){
+            if (pos != 0){
+                for (int i = pos - 1; i >= 0; i--){
+                    printf("%c", words[i]);
+                }
+                printf("\n");
+                pos = 0;
+            }
+        }
+        else{
+            words[pos] = c;
+            pos++;
+        }
+    }
     return 0;
 }
