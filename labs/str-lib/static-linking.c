a59175931f9281b04181a1cc676c0b8f4242f6a1
Luis Eduardo Nunez Altamirano <a01633894@itesm.mx>
2020-09-06 22:42:41 -0500

    Str-lib lab solved
    Signed-off-by: Luis Eduardo Nunez Altamirano <a01633894@itesm.mx>

--- labs/str-lib/static-linking.c
+++ labs/str-lib/static-linking.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main() {
+int mystrlen(char *str);
+char *mystradd(char *origin, char *addition);
+int mystrfind(char *origin, char *substr);
+
+int main(int argc, char **argv){
+    if (argc !=4){
+        printf("Not the correct number of arguments\n");
+        return 0;
+    }
+    char *instruction=argv[1];
+    char *string1 =argv[2];
+    char *string2=argv[3];
+    
+    if(strcmp(instruction,"-add")==0){
+        char *newString=mystradd(string1,string2);
+    }else if(strcmp(instruction,"-find")==0){
+        mystrfind(string1,string2);
+    }
+
     return 0;
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,52 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-
-int mystrlen(char *str){
-    return 0;
+int mystrlen(char *str)
+{
+    int size = 0;
+    while (str[size] != NULL)
+    {
+        size++;
+    }
+    return size;
 }
 
-char *mystradd(char *origin, char *addition){
-    return 0;
+char *mystradd(char *origin, char *addition)
+{
+    char *newString = malloc(mystrlen(origin) + mystrlen(addition));
+    int size1 = mystrlen(origin);
+    int size2 = mystrlen(addition);
+    for (int i = 0; i < size1; i++)
+    {
+        newString[i] = origin[i];
+    }
+
+    int i = 0;
+    for (int j = size1; j < size2 + size1; j++)
+    {
+        newString[j] = addition[i];
+        i++;
+    }
+    printf("Initial Lenght: %d\n", mystrlen(origin));
+    printf("New String: %s\n", newString);
+    printf("New Lenght: %d\n", mystrlen(newString));
+    return(0);
 }
 
 int mystrfind(char *origin, char *substr){
+	int pos=0;
+    while (*origin!= NULL) {
+        while(*origin==*substr){
+            origin++,substr++;
+            if(*substr==NULL){
+               printf("%s string was found at [%d] position\n",substr,pos);
+               return 0;
+            }
+        }
+        substr=&substr[0],pos++,origin++;
+    }
+    printf("%s string was not found\n",substr);
     return 0;
+
 }
