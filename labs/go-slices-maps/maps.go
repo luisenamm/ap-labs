602d4188453f65719d797e606f12a2d1a2448c04
Luis Eduardo Nunez Altamirano <a01633894@itesm.mx>
2020-09-18 12:59:18 -0500

    Go-slices-maps lab solved
    Signed-off-by: Luis Eduardo Nunez Altamirano <a01633894@itesm.mx>

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,20 @@
 package main
 
+import (
+	"strings"
+
+	"golang.org/x/tour/wc"
+)
+
+func WordCount(s string) map[string]int {
+	str := strings.Fields(s)
+	mapa := make(map[string]int)
+	for _, i := range str {
+		mapa[i]++
+	}
+	return mapa
+}
+
 func main() {
+	wc.Test(WordCount)
 }
--- labs/go-slices-maps/slices.go
+++ labs/go-slices-maps/slices.go
@@ -3,7 +3,14 @@ package main
 import "golang.org/x/tour/pic"
 
 func Pic(dx, dy int) [][]uint8 {
-     return [][]uint8{}
+	pic := make([][]uint8, dx)
+	for x := 0; x < dx; x++ {
+		pic[x] = make([]uint8, dy)
+		for y := 0; y < dy; y++ {
+			pic[x][y] = uint8(x * y)
+		}
+	}
+	return pic
 }
 
 func main() {
