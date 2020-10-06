8c1ffb4d73d6bc7537b3a32879147e7cf45f5501
Luis Eduardo Nunez Altamirano <a01633894@itesm.mx>
2020-10-06 18:15:04 -0500

    File-dir-monitor lab solved
    Signed-off-by: Luis Eduardo Nunez Altamirano <a01633894@itesm.mx>

--- labs/file-dir-monitor/logger.c
+++ labs/file-dir-monitor/logger.c
@@ -1,23 +1,113 @@
 #include <stdio.h>
 #include "logger.h"
+#include <stdarg.h>
+#include <string.h>
+#include <signal.h>
+#include <syslog.h>
+
+#define RESET 0
+#define BRIGHT 1
+#define DIM 2
+#define UNDERLINE 3
+#define BLINK 4
+#define REVERSE 7
+#define HIDDEN 8
+
+#define BLACK 0
+#define RED 1
+#define GREEN 2
+#define YELLOW 3 
+#define BLUE 4
+#define MAGENTA 5
+#define CYAN 6
+#define WHITE 7
+
+int mySysLog=0;
 
 int initLogger(char *logType) {
-    printf("Initializing Logger on: %s\n", logType);
+    if(strcmp("syslog",logType)==0){
+        mySysLog=1;
+        printf("Initializing Logger on: %s\n", logType);
+    }else if(strcmp("",logType)==0 || strcmp(logType,"stdout")==0)
+        printf("Initializing Logger on: stdout\n");
+    else{
+        printf("Not valid log type\n");
+        return 0;
+    }
     return 0;
 }
 
+
+void textColor(int attr, int fg,int bg){
+    char command[13];
+    sprintf(command,"%c[%d;%d;%dm",0x1B,attr,fg+30,bg+40);
+    printf("%s",command);
+}
+
 int infof(const char *format, ...) {
+    textColor(BRIGHT,BLUE,BLACK);
+    va_list arg;
+    va_start(arg,format);
+
+    if(mySysLog==1){
+        vsyslog(LOG_INFO,format,arg);
+        closelog();
+    }else{
+        vprintf(format,arg);
+        printf("\n");
+    }
+    va_end(arg);
+    textColor(RESET,WHITE,BLACK);
     return 0;
 }
 
 int warnf(const char *format, ...) {
+    textColor(BRIGHT,YELLOW,BLACK);
+    va_list arg;
+    va_start(arg,format);
+
+    if(mySysLog==1){
+        vsyslog(LOG_WARNING,format,arg);
+        closelog();
+    }else{
+        vprintf(format,arg);
+        printf("\n");
+    }
+    va_end(arg);
+    textColor(RESET,WHITE,BLACK);
     return 0;
 }
 
 int errorf(const char *format, ...) {
+    textColor(BRIGHT,RED,BLACK);
+    va_list arg;
+    va_start(arg,format);
+
+    if(mySysLog==1){
+        vsyslog(LOG_ERR,format,arg);
+        closelog();
+    }else{
+        vprintf(format,arg);
+        printf("\n");
+    }
+    va_end(arg);
+    textColor(RESET,WHITE,BLACK);
     return 0;
 }
 int panicf(const char *format, ...) {
+    textColor(BLINK,RED,WHITE);
+    va_list arg;
+    va_start(arg,format);
+
+    if(mySysLog==1){
+        vsyslog(LOG_EMERG,format,arg);
+        closelog();
+    }else{
+        vprintf(format,arg);
+        printf("\n");
+    }
+    va_end(arg);
+    textColor(RESET,WHITE,BLACK);
+    raise(SIGABRT);
     return 0;
 }
-
--- labs/file-dir-monitor/monitor.c
+++ labs/file-dir-monitor/monitor.c
@@ -1,7 +1,76 @@
+#define _XOPEN_SOURCE 500
+#define BUF_LEN (10*(sizeof(struct inotify_event)+100))
+
 #include <stdio.h>
 #include "logger.h"
+#include <ftw.h>
+#include <sys/inotify.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <unistd.h>
 
-int main(){
+int inotifyFd,ld, fdLimit=15;
+int flags = FTW_CHDIR | FTW_DEPTH | FTW_MOUNT;
+
+char* subDir[300],decriptor[300], lastName;
+
+static int display_info(const char* fpath,const struct stat *sb,int tflag,struct FTW *ftwbuf){
+    if(tflag==FTW_D){
+        ld=inotify_add_watch(inotifyFd,fpath,IN_CREATE|IN_DELETE|IN_MOVE);
+        if(ld==-1)
+            ferror("inotify_add_watch");
+    }
+    return 0;
+}
+
+static void displayInotifyEvent(struct inotify_event *i){
+    if(i->mask & IN_CREATE)  infof("Created %s\n",i->name);
+    if(i->mask & IN_DELETE)  infof("Deleted %s\n",i->name);
+    if(i->mask & IN_MOVED_FROM)  infof("Moving from %s\n",i->name);
+    if(i->mask & IN_MOVED_TO) infof("Moving to %s\n",i->name);
+}
+
+
+int main(int argc, char *argv[]){
     // Place your magic here
+    int flags=0;
+    char buf[BUF_LEN] __attribute__ ((aligned(8)));
+    ssize_t numRead;
+    char* p;
+    struct inotify_event *event;
+
+    inotifyFd = inotify_init();
+    if (inotifyFd == -1)
+        perror("inotify_init");
+
+    if (argc > 2 && strchr(argv[2], 'd') != NULL)
+        flags |= FTW_DEPTH;
+    if (argc > 2 && strchr(argv[2], 'p') != NULL)
+        flags |= FTW_PHYS;
+    
+    if (nftw((argc < 2) ? "." : argv[1], display_info, 20, flags) == -1){
+        perror("nftw");
+        exit(EXIT_FAILURE);
+    }
+    if (argc < 2 || strcmp(argv[1], "--help") == 0){
+        errorf("Usage is: %s <PATHNAME>\n", argv[0]);
+        return 0;
+    }
+
+    for(;;){
+        numRead=read(inotifyFd,buf,BUF_LEN);
+        if(numRead==0)
+            perror("read() from inotify fd returned 0");
+        if(numRead==-1)
+            perror("read");
+        for(p=buf;p<buf+numRead;){
+            event=(struct inotify_event *) p;
+            displayInotifyEvent(event);
+            p+=sizeof(struct inotify_event)+event->len;
+        }
+    }
+
+    exit(EXIT_SUCCESS);
     return 0;
 }
