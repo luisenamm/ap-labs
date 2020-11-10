c191f6da82b79aff98616859cb1f73edcce3973b
Luis Eduardo Nunez Altamirano <a01633894@itesm.mx>
2020-11-10 09:07:34 -0600

    Goroutines-vs-os-threads lab solved
    Signed-off-by: Luis Eduardo Nunez Altamirano <a01633894@itesm.mx>

--- labs/goroutines-vs-os-threads/ping-pong.go
+++ labs/goroutines-vs-os-threads/ping-pong.go
@@ -2,10 +2,30 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
+func connection(ping chan int, pong chan int) {
+	for {
+		pong <- (1 + <-ping)
+	}
+}
+
 func main() {
-	var commsPerSecond int
+	var commsPerSecond int = 0
+
+	ping := make(chan int)
+	pong := make(chan int)
+
+	go connection(ping, pong)
+	go connection(pong, ping)
+
+	for i := 0; i < 1; i++ {
+		ping <- 0
+		time.Sleep(time.Duration(1) * time.Second)
+		cont := <-ping
+		commsPerSecond += cont
+	}
 
 	fmt.Println("Communications Per Second : ", commsPerSecond)
 }
--- labs/goroutines-vs-os-threads/pipeline.go
+++ labs/goroutines-vs-os-threads/pipeline.go
@@ -1,12 +1,52 @@
+//I worked with 1,000,000 pipes
 package main
 
 import (
 	"fmt"
+	"runtime"
+	"time"
 )
 
-func main() {
-	var maxStages, transitTime int
+var maxStages int
+var transitTime float64
+var first chan int
 
-	fmt.Println("Maximum number of pipeline stages   : ", maxStages)
-	fmt.Println("Time to transit trough the pipeline : ", transitTime)
+//https://golangcode.com/print-the-current-memory-usage/
+func printMemUsage() {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	if m.TotalAlloc/1024/1024 > 1024 {
+		fmt.Sprintf("Out of memory")
+	}
+}
+
+func pipes(maxPipes int) {
+	first = make(chan int)
+	last := first
+	i := 0
+	for i < maxPipes {
+		maxStages++
+		printMemUsage()
+		aux := last
+		go func() {
+			for data := range aux {
+				last <- data
+			}
+		}()
+		i++
+	}
+}
+
+func testPipes() {
+	start := time.Now()
+	pipes(1000000)
+	finish := time.Now()
+	transitTime = finish.Sub(start).Seconds()
+}
+
+func main() {
+	testPipes()
+	fmt.Println("Maximum number of pipeline stages: ", maxStages)
+	fmt.Println("Total time to traverse the entire pipeline: ", transitTime, " seconds")
+
 }
